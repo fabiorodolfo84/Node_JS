8bf46fca1314e93367c47201fad5da63f023d730
fabiorodolfo1984@gmail.com <fabiorodolfo1984@gmail.com>
2023-01-11 14:54:57 -0300

    update

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 
 // Estou dizendo para o Express usar o EJS com o View Engine.
-
 app.set('view engine','ejs');
+app.use(express.static('public'));
+
+//Rotas
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 app.get("/",(req, res) => {
-    let nome = "Fabio Rodolfo";
-    let lang = "Javascript";
-    res.render("index",{
-        nome: nome, 
-        lang: lang, 
-        empresa: "SnakeTattoo"});
+    res.render("index")
+        
     });
+
+ app.get("/perguntar",(req, res)=>{
+    res.render("perguntar");
+ });
+ 
+ app.post("/salvarpergunta",(req, res) =>{
+   var titulo = req.body.titulo;
+   var descricao = req.body.descricao;
+   
+
+   res.send("Formulario recebido! titulo " + " " + " descricao " + descricao );
+   
+  });
 
 
 app.listen(8080,()=>{
-    console.log("App rodando!");
-});
+    console.log("App rodando!");});
+
 
 
 
